4ba66d2adb8bcfc02dc722177e7ff00a619e4da5
Nicky <asdasd>
2022-10-13 00:23:28 -0300

    bingo

--- lib/folding_cardboard_component_custom.dart
+++ lib/folding_cardboard_component_custom.dart
@@ -2,10 +2,11 @@ import 'package:flutter/material.dart';
 import 'models/bingo_ticket_model.dart';
 
 class FoldingCardboardComponentCustom extends StatefulWidget {
-  final Color colorMain;
+  final Color mainColor;
+  final Color iconColor;
   final Color backgroundColor;
   final Color cellColor;
-  final Color cellText;
+  final Color cellColorText;
   final String title;
   final BingoTicketModel bingoTicketModel;
   final EdgeInsetsGeometry padding;
@@ -16,11 +17,12 @@ class FoldingCardboardComponentCustom extends StatefulWidget {
     Key? key,
     this.title = "Número de cartón",
     required this.bingoTicketModel,
-    this.colorMain = const Color.fromARGB(255, 33, 82, 243),
+    this.mainColor = const Color.fromARGB(255, 33, 82, 243),
+    this.iconColor = Colors.blue,
     this.backgroundColor = Colors.white,
-    this.cellText = Colors.black54,
+    this.cellColorText = Colors.black54,
     this.cellColor = const Color.fromARGB(255, 239, 239, 239),
-    this.padding = const EdgeInsets.all(8),
+    this.padding = const EdgeInsets.all(0),
     this.borderRadiusCircularCardBoard = 12,
     this.borderRadiusCircularCell = 6,
   }) : super(key: key);
@@ -38,8 +40,8 @@ class _FoldingCardboardComponentCustomState
   @override
   Widget build(BuildContext context) {
     final double size = MediaQuery.of(context).size.width.toDouble();
-    late final double sizeCell = size / 9 >= 52 ? 52 : size / 9;
-    late final double sizeNumber = size / 9 >= 52 ? 19.5 : size / 24;
+    late final double sizeCell = size / 9;
+    late final double sizeNumber = size / 24;
     late final double sizeText = size / 9 >= 52 ? 19.5 : size / 24;
     late final double sizeContainerMainNumber =
         size / 5.2 >= 90 ? 90 : size / 5.2;
@@ -58,41 +60,39 @@ class _FoldingCardboardComponentCustomState
 
   void onTap() {
     setState(() {
-      if (!_isOpen) {
-        sizeTable = (MediaQuery.of(context).size.width.toDouble() / 9 >= 52
-                ? 52
-                : (MediaQuery.of(context).size.width.toDouble() / 9) * 5) +
-            55;
-      } else {
-        sizeTable = 0;
-      }
       _isOpen = !_isOpen;
     });
   }
 
   _cell(index, sizeCell, sizeNumber) {
-    return Container(
-      width: sizeCell,
-      height: sizeCell,
-      margin: const EdgeInsets.all(
-          5), //index%7 == 0 ? const EdgeInsets.only(right: 5) : const EdgeInsets.only(left: 5),
-      decoration: BoxDecoration(
-          color: widget.cellColor, //Colors.grey.shade200
-          borderRadius: BorderRadius.circular(widget.borderRadiusCircularCell),
-          boxShadow: [
-            BoxShadow(
-              color: widget.cellColor.withOpacity(0.9),
-              blurRadius: 2,
-              offset: const Offset(3.5, 3.5),
-            ),
-          ]),
-      alignment: Alignment.center,
-      child: Text(
-        addNumbers(index),
-        style: TextStyle(
-          fontWeight: FontWeight.bold,
-          fontSize: sizeNumber,
-          color: widget.cellText, //Colors.black54
+    return Padding(
+      padding: const EdgeInsets.only(
+        right: 12,
+        bottom: 12,
+      ),
+      child: Container(
+        width: sizeCell,
+        height: sizeCell,
+        //margin: const EdgeInsets.all(6), //index%7 == 0 ? const EdgeInsets.only(right: 5) : const EdgeInsets.only(left: 5),
+        decoration: BoxDecoration(
+            color: widget.cellColor, //Colors.grey.shade200
+            borderRadius:
+                BorderRadius.circular(widget.borderRadiusCircularCell),
+            boxShadow: [
+              BoxShadow(
+                color: Colors.grey.shade400.withOpacity(0.5),
+                blurRadius: 1,
+                offset: const Offset(1, 1),
+              ),
+            ]),
+        alignment: Alignment.center,
+        child: Text(
+          addNumbers(index),
+          style: TextStyle(
+            fontWeight: FontWeight.normal,
+            fontSize: sizeNumber,
+            color: widget.cellColorText, //Colors.black54
+          ),
         ),
       ),
     );
@@ -129,83 +129,82 @@ class _FoldingCardboardComponentCustomState
             decoration: BoxDecoration(
               color: widget.backgroundColor,
             ),
-            child: Padding(
-              padding: widget.padding, //const EdgeInsets.symmetric(vertical: 10, horizontal: 15),
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Padding(
-                    padding: const EdgeInsets.only(right: 0, left: 5),
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [
-                        Text(
-                          widget.title,
-                          style: TextStyle(
-                            fontSize: sizeText,
-                            fontWeight: FontWeight.w500,
-                            color: widget.colorMain, //colorMain
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Padding(
+                  padding: const EdgeInsets.only(left: 15, top: 5, bottom: 5),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text(
+                        widget.title,
+                        style: TextStyle(
+                          fontSize: sizeText,
+                          fontWeight: FontWeight.w500,
+                          color: widget.mainColor, //colorMain
+                        ),
+                      ),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Padding(
+                            padding: const EdgeInsets.only(right: 8.0),
+                            child: Container(
+                              height: sizeContainerMainNumber / 2.5,
+                              width: sizeContainerMainNumber,
+                              alignment: Alignment.center,
+                              decoration: BoxDecoration(
+                                border: Border.all(
+                                  width: 2,
+                                  color: widget.mainColor, //colorMain
+                                ),
+                                borderRadius: BorderRadius.circular(20),
+                              ),
+                              child: Text(
+                                widget.bingoTicketModel.number.toString(),
+                                style: TextStyle(
+                                  color: widget.mainColor, //colorMain
+                                  fontWeight: FontWeight.w600,
+                                  fontSize: sizeText / 1.15,
+                                  letterSpacing: 1,
+                                ),
+                              ),
+                            ),
                           ),
-                        ),
-                        Row(
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          children: [
-                            Padding(
-                              padding: const EdgeInsets.only(right: 8.0),
-                              child: Container(
-                                height: sizeContainerMainNumber / 2.5,
-                                width: sizeContainerMainNumber,
-                                alignment: Alignment.center,
-                                decoration: BoxDecoration(
-                                  border: Border.all(
-                                    width: 2,
-                                    color: widget.colorMain, //colorMain
-                                  ),
-                                  borderRadius: BorderRadius.circular(20),
-                                ),
-                                child: Text(
-                                  widget.bingoTicketModel.number.toString(),
-                                  style: TextStyle(
-                                    color: widget.colorMain, //colorMain
-                                    fontWeight: FontWeight.w600,
-                                    fontSize: sizeText / 1.15,
-                                    letterSpacing: 1,
-                                  ),
-                                ),
-                              ),
+                          AnimatedRotation(
+                            turns: _isOpen ? -0.5 : 0,
+                            duration: const Duration(milliseconds: 250),
+                            child: Icon(
+                              Icons.arrow_drop_down,
+                              size: sizeIcon,
+                              color: widget.iconColor,
                             ),
-                            AnimatedRotation(
-                              turns: _isOpen ? -0.5 : 0,
-                              duration: const Duration(milliseconds: 250),
-                              child: Icon(
-                                Icons.arrow_drop_down,
-                                size: sizeIcon,
-                                color: widget.colorMain,
-                              ),
-                            ),
-                          ],
-                        ),
-                      ],
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
+                ),
+                AnimatedContainer(
+                  duration: const Duration(milliseconds: 250),
+                  curve: Curves.linear,
+                  height: _isOpen
+                      ? sizeCell * 5 + 74
+                      : 0, //sizeCell*5+55, //(MediaQuery.of(context).size.width.toDouble() / 9 >= 52 ? 52 : (MediaQuery.of(context).size.width.toDouble() / 9)*5) + 55,//_isVisible ? sizeTable : (MediaQuery.of(context).size.width.toDouble() / 9 >= 52 ? 52 : (MediaQuery.of(context).size.width.toDouble() / 9)*5) + 55,
+                  child: Padding(
+                    padding: const EdgeInsets.only(
+                      top: 10,
+                      bottom: 3,
+                      right: 3,
+                      left: 15,
+                    ), //const EdgeInsets.only(top: 10,left: 5,right: 10),
+                    child: Table(
+                      children: cardboard,
                     ),
                   ),
-                  AnimatedContainer(
-                    duration: const Duration(milliseconds: 250),
-                    curve: Curves.linear,
-                    height: _isOpen
-                        ? sizeCell * 5 + 55
-                        : 0, //sizeCell*5+55, //(MediaQuery.of(context).size.width.toDouble() / 9 >= 52 ? 52 : (MediaQuery.of(context).size.width.toDouble() / 9)*5) + 55,//_isVisible ? sizeTable : (MediaQuery.of(context).size.width.toDouble() / 9 >= 52 ? 52 : (MediaQuery.of(context).size.width.toDouble() / 9)*5) + 55,
-                    child: Padding(
-                      padding: EdgeInsets.only(
-                          top: widget.padding.vertical / 3,
-                          right:
-                              5), //const EdgeInsets.only(top: 10,left: 5,right: 10),
-                      child: Table(
-                        children: cardboard,
-                      ),
-                    ),
-                  )
-                ],
-              ),
+                )
+              ],
             ),
           ),
         ),
--- lib/view.dart
+++ lib/view.dart
@@ -5,6 +5,7 @@ import 'models/bingo_ticket_model.dart';
 
 class View extends StatefulWidget {
   final String id = "view";
+  final bool changeCard = true;
   const View({Key? key}) : super(key: key);
 
   @override
@@ -32,12 +33,12 @@ class _ViewState extends State<View> {
     List<int> _numbersList() {
       List<int> numbers = [];
       for (int i = 0; i < 33; i++) {
-        numbers.add(_numberUnique(100,numbers));
+        numbers.add(_numberUnique(90,numbers));
       }
       return numbers;
     }
 
-    BingoTicketModel lotteryTicket =
+    BingoTicketModel lotteryTicketA =
         BingoTicketModel(number: 106044, numberList: _numbersList());
     BingoTicketModel lotteryTicketB =
         BingoTicketModel(number: 106040, numberList: _numbersList());
@@ -45,60 +46,149 @@ class _ViewState extends State<View> {
         BingoTicketModel(number: 109033, numberList: _numbersList());
     BingoTicketModel lotteryTicketD =
         BingoTicketModel(number: 102099, numberList: _numbersList());
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text("Bingooo"),
-      ),
-      body: Container(
-        decoration: const BoxDecoration(
-          color: Colors.grey, //Colors.white38
-          gradient: LinearGradient(
-            colors: [Colors.pink,Colors.white],
-            begin: FractionalOffset.topCenter,
-            end: FractionalOffset.bottomCenter,
-          )
+    return SafeArea(
+      child: Scaffold(
+        floatingActionButton: FloatingActionButton(
+          backgroundColor: Colors.black,
+          onPressed: onTap,
+          child: const Icon(
+            Icons.compare_arrows_sharp,
+            color: Colors.purple,
+          ),
         ),
-        child: SingleChildScrollView(
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.center,
+        body: Container(
+          width: double.infinity,
+          height: double.infinity,
+          decoration: const BoxDecoration( //Colors.white38
+            gradient: LinearGradient(
+              colors: [Colors.pink,Colors.white],
+              begin: FractionalOffset.centerLeft,
+              end: FractionalOffset.centerRight,
+            )
+          ),
+          child: Stack(
+            alignment: Alignment.center,
             children: [
-              FoldingCardboardComponentCustom(
-                  bingoTicketModel: lotteryTicket,
-              ),
-              const SizedBox(
-                height: 30,
-              ),
-              Padding(
-                padding: const EdgeInsets.all(12.0),
-                child: FoldingCardboardComponentCustom(
-                    bingoTicketModel: lotteryTicketB,)
-              ),
-              const SizedBox(
-                height: 30,
-              ),
-              Padding(
-                padding: const EdgeInsets.all(12.0),
-                child: FoldingCardboardComponentCustom(
-                    bingoTicketModel: lotteryTicketC,
-                    padding: const EdgeInsets.all(20),),
-              ),
-              Padding(
-                padding: const EdgeInsets.all(12.0),
-                child: FoldingCardboardComponentCustom(
-                    bingoTicketModel: lotteryTicketD,
-                    backgroundColor: Colors.blueAccent,
-                    borderRadiusCircularCardBoard: 24,
-                    borderRadiusCircularCell: 12,
-                    cellColor: Colors.black,
-                    cellText: Colors.white,
-                    colorMain: Colors.white,
-                    title: "Otro titulo para la tarjeta",
-                    padding: const EdgeInsets.all(12),
-                ),
-              ),
+              _editedCardboard(lotteryTicketA, lotteryTicketB, lotteryTicketC, lotteryTicketD),
+              _cardboard(lotteryTicketA, lotteryTicketB, lotteryTicketC, lotteryTicketD),
             ],
           ),
         ),
+      ),
+    );
+  }
+  bool changeEdit = false;
+  bool change = true;
+  void onTap(){
+    setState(() {
+      changeEdit = !changeEdit;
+      change = !change;
+    });
+  }
+  _editedCardboard(lotteryTicketA,lotteryTicketB,lotteryTicketC,lotteryTicketD){
+    return Visibility(
+      visible: changeEdit,
+      child: SingleChildScrollView(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            FoldingCardboardComponentCustom(
+              bingoTicketModel: lotteryTicketA,
+            ),
+            const SizedBox(
+              height: 30,
+            ),
+            Padding(
+                padding: const EdgeInsets.all(12.0),
+                child: FoldingCardboardComponentCustom(
+                  bingoTicketModel: lotteryTicketB,)
+            ),
+            const SizedBox(
+              height: 30,
+            ),
+            Padding(
+              padding: const EdgeInsets.all(12.0),
+              child: FoldingCardboardComponentCustom(
+                bingoTicketModel: lotteryTicketC,
+                padding: const EdgeInsets.all(20),),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(12.0),
+              child: FoldingCardboardComponentCustom(
+                bingoTicketModel: lotteryTicketD,
+                backgroundColor: Colors.blueAccent,
+                borderRadiusCircularCardBoard: 24,
+                borderRadiusCircularCell: 12,
+                cellColor: Colors.black,
+                cellColorText: Colors.white,
+                mainColor: Colors.white,
+                title: "Otro titulo para la tarjeta",
+                padding: const EdgeInsets.all(12),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+  _cardboard(lotteryTicketA,lotteryTicketB,lotteryTicketC,lotteryTicketD){
+    return Visibility(
+      visible: change,
+      child: ClipRRect(
+        borderRadius: BorderRadius.circular(20),
+        child: Container(
+          width: double.infinity,
+          height: 350,
+          decoration: const BoxDecoration(
+            color: Colors.white54,
+          ),
+          child: SingleChildScrollView(
+            child: Padding(
+              padding: const EdgeInsets.symmetric(vertical: 15.0),
+              child: Column(
+                children: [
+                  Text(
+                    "BINGO",
+                    style: TextStyle(
+                      fontSize: 30,
+                      color: Colors.blue.shade800,
+                      letterSpacing: 3,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  const SizedBox(height: 30,),
+                  SingleChildScrollView(
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: [
+                        _card(lotteryTicketA),
+                        const SizedBox(
+                          height: 30,
+                        ),_card(lotteryTicketB),
+                        const SizedBox(
+                          height: 30,
+                        ),_card(lotteryTicketC),
+                        const SizedBox(
+                          height: 30,
+                        ),_card(lotteryTicketD),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+  _card (lotteryTicket){
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 20.0),
+      child: FoldingCardboardComponentCustom(
+        bingoTicketModel: lotteryTicket,
+        mainColor: Colors.blue.shade900,
+        iconColor: Colors.blue.shade800,
       ),
     );
   }
